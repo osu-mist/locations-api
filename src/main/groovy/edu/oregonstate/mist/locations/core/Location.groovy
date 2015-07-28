09e2a24e2ab72e29169643376806a7f0b93e62e5
floppyears <cedenoj@onid.oregonstate.edu>
2015-07-28 13:56:29 -0700

    Add location pogo
    CO-156

---
+++ src/main/groovy/edu/oregonstate/mist/locations/core/Location.groovy
@@ -0,0 +1,15 @@
+package edu.oregonstate.mist.locations.core
+
+class Location {
+    Integer id
+    String name
+    String abbrev
+    String shortDescription
+    String description
+    String address
+    String latitude
+    String longitude
+    String adaEntrance
+    String thumbnail
+    String largerImage
+}
