5329bc0324ed8f3411bc0a023d5a54be9aea56ec
floppyears <cedenoj@onid.oregonstate.edu>
2016-01-03 19:19:03 -0800

    Change DW resource method return type
    CO-259
    
    Using resource build to specify the correct headers to return on
    a successful request.

--- src/main/groovy/edu/oregonstate/mist/locations/resources/LocationResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/locations/resources/LocationResource.groovy
@@ -8,7 +8,6 @@ import edu.oregonstate.mist.locations.core.DiningLocation
 import edu.oregonstate.mist.locations.db.CampusMapLocationDAO
 import edu.oregonstate.mist.locations.db.DiningDAO
 import edu.oregonstate.mist.locations.db.LocationDAO
-import edu.oregonstate.mist.locations.jsonapi.ResourceObject
 import edu.oregonstate.mist.locations.jsonapi.ResultObject
 import io.dropwizard.auth.Auth
 
@@ -35,27 +34,27 @@ class LocationResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Timed
-    List<CampusMapLocation> getCampusMap(@Auth AuthenticatedUser authenticatedUser) {
+    Response getCampusMap(@Auth AuthenticatedUser authenticatedUser) {
         final List<CampusMapLocation> campusMapLocations = campusMapLocationDAO.getCampusMapLocations()
 
         if (!campusMapLocations) {
             throw new WebApplicationException(Response.Status.NOT_FOUND)
         }
 
-        campusMapLocations
+        Response.ok(campusMapLocations).build()
     }
 
     @GET
     @Path("dining")
     @Produces(MediaType.APPLICATION_JSON)
     @Timed
-    List<DiningLocation> getDining(@Auth AuthenticatedUser authenticatedUser) {
+    Response getDining(@Auth AuthenticatedUser authenticatedUser) {
         final List<DiningLocation> diningLocations = diningDAO.getDiningLocations()
 
         if (!diningLocations) {
             throw new WebApplicationException(Response.Status.NOT_FOUND)
         }
 
-        diningLocations
+        Response.ok(diningLocations).build()
     }
 }
