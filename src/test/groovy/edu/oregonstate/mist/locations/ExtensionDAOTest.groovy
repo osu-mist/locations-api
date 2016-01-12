8b1387b066ee9acde2672b82c924e85d4e0912e9
floppyears <cedenoj@onid.oregonstate.edu>
2016-01-12 13:24:56 -0800

    Add extension DAO test
    CO-259
    
    Test fetches extension data and verifies that the fields are set
    properly.

---
+++ src/test/groovy/edu/oregonstate/mist/locations/ExtensionDAOTest.groovy
@@ -0,0 +1,42 @@
+package edu.oregonstate.mist.locations
+
+import edu.oregonstate.mist.locations.core.ExtensionLocation
+import edu.oregonstate.mist.locations.db.ExtensionDAO
+import io.dropwizard.testing.junit.DropwizardAppRule
+import org.junit.BeforeClass
+import org.junit.ClassRule
+import org.junit.Test
+
+class ExtensionDAOTest {
+    private static ExtensionDAO extensionDAO
+    private static LocationUtil locationUtil
+    private static List<ExtensionLocation> extensionLocations
+
+    @ClassRule
+    public static final DropwizardAppRule<LocationConfiguration> APPLICATION =
+            new DropwizardAppRule<LocationConfiguration>(
+                    LocationApplication.class,
+                    new File("configuration.yaml").absolutePath)
+
+    @BeforeClass
+    public static void setUpClass() {
+        locationUtil = new LocationUtil(APPLICATION.configuration.locationsConfiguration)
+        extensionDAO = new ExtensionDAO(APPLICATION.configuration.locationsConfiguration, locationUtil)
+        extensionLocations = extensionDAO.getExtensionLocations()
+    }
+
+    @Test
+    public void testGetLocations() {
+        assert extensionLocations.size() != 0
+
+        extensionLocations.each { extensionLocation ->
+            assert extensionLocation.county != null
+            assert extensionLocation.zipCode != null
+            assert extensionLocation.fax != null
+            assert extensionLocation.locationUrl != null
+
+            assert extensionLocation.latitude.matches("-?\\d+(\\.\\d+)?")
+            assert extensionLocation.longitude.matches("-?\\d+(\\.\\d+)?")
+        }
+    }
+}
