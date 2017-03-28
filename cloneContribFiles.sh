73760fc1181ba23465db5f593cda6346063a0af6
Jared Kosanovic <jared@Jareds-MacBook-Pro.local>
2017-03-28 10:41:01 -0700

    quote the directory, because some directories have spaces

--- cloneContribFiles.sh
+++ cloneContribFiles.sh
@@ -5,6 +5,6 @@ if [ "$2" = "yes" ]
         cd campusmap
         git reset --hard "$3"
         cd "$WORKDIR"
-        mv campusmap/campusmap.json $WORKDIR/campusmap.json
+        mv campusmap/campusmap.json "$WORKDIR"/campusmap.json
         rm -rf campusmap/
 fi
