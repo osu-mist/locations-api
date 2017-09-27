1de2388b7470a437b03d6496be8583d306267e97
Andrew Ekstedt <ekstedta@oregonstate.edu>
2017-09-27 09:34:59 -0700

    use the skeleton's base Application class
    CO-935
    
    The edu.oregonstate.mist.api.Application class performs a lot of common
    setup for us, rather than repeating it in every api.

--- src/main/groovy/edu/oregonstate/mist/locations/LocationApplication.groovy
+++ src/main/groovy/edu/oregonstate/mist/locations/LocationApplication.groovy
@@ -1,12 +1,6 @@
 package edu.oregonstate.mist.locations
 
-import de.thomaskrille.dropwizard_template_config.TemplateConfigBundle
-import edu.oregonstate.mist.api.AuthenticatedUser
-import edu.oregonstate.mist.api.BasicAuthenticator
-import edu.oregonstate.mist.api.BuildInfoManager
-import edu.oregonstate.mist.api.Configuration
-import edu.oregonstate.mist.api.Resource
-import edu.oregonstate.mist.api.InfoResource
+import edu.oregonstate.mist.api.Application
 import edu.oregonstate.mist.locations.db.ArcGisDAO
 import edu.oregonstate.mist.locations.db.CampusMapDAO
 import edu.oregonstate.mist.locations.db.ExtraDataDAO
@@ -21,57 +15,17 @@ import edu.oregonstate.mist.locations.health.DiningHealthCheck
 import edu.oregonstate.mist.locations.health.ExtensionHealthCheck
 import edu.oregonstate.mist.locations.health.LibraryHealthCheck
 import edu.oregonstate.mist.locations.resources.LocationResource
-import edu.oregonstate.mist.api.PrettyPrintResponseFilter
-import edu.oregonstate.mist.api.jsonapi.GenericExceptionMapper
-import edu.oregonstate.mist.api.jsonapi.NotFoundExceptionMapper
-import io.dropwizard.Application
-import io.dropwizard.auth.AuthDynamicFeature
-import io.dropwizard.auth.AuthValueFactoryProvider
-import io.dropwizard.auth.basic.BasicCredentialAuthFilter
 import io.dropwizard.client.HttpClientBuilder
 import io.dropwizard.jdbi.DBIFactory
-import io.dropwizard.jersey.errors.LoggingExceptionMapper
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import org.apache.http.client.HttpClient
 import org.skife.jdbi.v2.DBI
 
-import javax.ws.rs.WebApplicationException
-
 /**
  * Main application class.
  */
 class LocationApplication extends Application<LocationConfiguration> {
-    /**
-     * Initializes application bootstrap.
-     *
-     * @param bootstrap
-     */
-    @Override
-    public void initialize(Bootstrap<Configuration> bootstrap) {
-        bootstrap.addBundle(new TemplateConfigBundle())
-    }
-
-    /**
-     * Registers lifecycle managers and Jersey exception mappers
-     * and container response filters
-     *
-     * @param environment
-     * @param buildInfoManager
-     */
-    protected void registerAppManagerLogic(Environment environment,
-                                           BuildInfoManager buildInfoManager,
-                                           ExtraDataManager extraDataManager) {
-
-        environment.lifecycle().manage(buildInfoManager)
-        environment.lifecycle().manage(extraDataManager)
-
-        environment.jersey().register(new NotFoundExceptionMapper())
-        environment.jersey().register(new GenericExceptionMapper())
-        environment.jersey().register(new LoggingExceptionMapper<WebApplicationException>(){})
-        environment.jersey().register(new PrettyPrintResponseFilter())
-    }
-
     /**
      * Parses command-line arguments and runs the application.
      *
@@ -80,10 +34,10 @@ class LocationApplication extends Application<LocationConfiguration> {
      */
     @Override
     public void run(LocationConfiguration configuration, Environment environment) {
-        Resource.loadProperties()
-        BuildInfoManager buildInfoManager = new BuildInfoManager()
+        this.setup(configuration, environment)
+
         ExtraDataManager extraDataManager = new ExtraDataManager()
-        registerAppManagerLogic(environment, buildInfoManager, extraDataManager)
+        environment.lifecycle().manage(extraDataManager)
 
         // the httpclient from DW provides with many metrics and config options
         HttpClient httpClient = new HttpClientBuilder(environment)
@@ -91,8 +45,7 @@ class LocationApplication extends Application<LocationConfiguration> {
                 .build("backend-http-client")
 
         def configMap = configuration.locationsConfiguration
-        final LibraryDAO libraryDAO = new LibraryDAO(configuration.locationsConfiguration,
-                httpClient)
+        final LibraryDAO libraryDAO = new LibraryDAO(configMap, httpClient)
         final LocationDAO locationDAO = new LocationDAO(configMap)
         final LocationUtil locationUtil = new LocationUtil(configMap)
         final ExtensionDAO extensionDAO = new ExtensionDAO(configMap, locationUtil)
@@ -114,15 +67,6 @@ class LocationApplication extends Application<LocationConfiguration> {
         environment.jersey().register(new LocationResource(diningDAO, locationDAO,
                 extensionDAO, arcGisDAO, extraDataDAO, extraDataManager, libraryDAO,
                 useHttpCampusMap, campusMapDAO, facilDAO))
-        environment.jersey().register(new InfoResource(buildInfoManager.getInfo()))
-        environment.jersey().register(new AuthDynamicFeature(
-                new BasicCredentialAuthFilter.Builder<AuthenticatedUser>()
-                      .setAuthenticator(new BasicAuthenticator(configuration.getCredentialsList()))
-                      .setRealm('LocationApplication')
-                      .buildAuthFilter()
-        ))
-        environment.jersey().register(new AuthValueFactoryProvider.Binder
-                <AuthenticatedUser>(AuthenticatedUser.class))
     }
 
     private static void addHealthChecks(Environment environment,
