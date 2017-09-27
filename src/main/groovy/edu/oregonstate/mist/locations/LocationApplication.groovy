35f6e14c50e5b4d11c3114ec16f1aab015a51d22
Andrew Ekstedt <ekstedta@oregonstate.edu>
2017-09-27 10:36:48 -0700

    [wip] working command

--- src/main/groovy/edu/oregonstate/mist/locations/LocationApplication.groovy
+++ src/main/groovy/edu/oregonstate/mist/locations/LocationApplication.groovy
@@ -1,6 +1,7 @@
 package edu.oregonstate.mist.locations
 
 import edu.oregonstate.mist.api.Application
+import edu.oregonstate.mist.locations.LocationCommand
 import edu.oregonstate.mist.locations.db.ArcGisDAO
 import edu.oregonstate.mist.locations.db.CampusMapDAO
 import edu.oregonstate.mist.locations.db.ExtraDataDAO
@@ -26,6 +27,12 @@ import org.skife.jdbi.v2.DBI
  * Main application class.
  */
 class LocationApplication extends Application<LocationConfiguration> {
+    @Override
+    public void initialize(Bootstrap bootstrap) {
+        super.initialize(bootstrap)
+        bootstrap.addCommand(new LocationCommand())
+    }
+
     /**
      * Parses command-line arguments and runs the application.
      *
---
+++ src/main/groovy/edu/oregonstate/mist/locations/LocationCommand.groovy
@@ -0,0 +1,206 @@
+package edu.oregonstate.mist.locations
+
+import com.codahale.metrics.annotation.Timed
+import com.fasterxml.jackson.databind.ObjectMapper
+import edu.oregonstate.mist.locations.LocationConfiguration
+import edu.oregonstate.mist.locations.core.ArcGisLocation
+import edu.oregonstate.mist.locations.core.CampusMapLocationDeprecated
+import edu.oregonstate.mist.locations.core.FacilLocation
+import edu.oregonstate.mist.locations.core.GenderInclusiveRRLocation
+import edu.oregonstate.mist.locations.core.ServiceLocation
+import edu.oregonstate.mist.locations.core.ExtensionLocation
+import edu.oregonstate.mist.locations.db.ArcGisDAO
+import edu.oregonstate.mist.locations.db.CampusMapDAO
+import edu.oregonstate.mist.locations.db.ExtraDataDAO
+import edu.oregonstate.mist.locations.db.DiningDAO
+import edu.oregonstate.mist.locations.db.ExtensionDAO
+import edu.oregonstate.mist.locations.db.ExtraDataManager
+import edu.oregonstate.mist.locations.db.FacilDAO
+import edu.oregonstate.mist.locations.db.LibraryDAO
+import edu.oregonstate.mist.locations.db.LocationDAO
+import edu.oregonstate.mist.api.jsonapi.ResultObject
+import io.dropwizard.cli.ConfiguredCommand
+import io.dropwizard.setup.Bootstrap
+import net.sourceforge.argparse4j.inf.Namespace
+import net.sourceforge.argparse4j.inf.Subparser
+
+class LocationCommand extends ConfiguredCommand<LocationConfiguration> {
+    private final DiningDAO diningDAO
+    private final LocationDAO locationDAO
+    private final ExtensionDAO extensionDAO
+    private final ArcGisDAO arcGisDAO
+    private final ExtraDataDAO extraDataDAO
+    private final LibraryDAO libraryDAO
+    private ExtraDataManager extraDataManager
+    private final Boolean useHttpCampusMap
+    private final CampusMapDAO campusMapDAO
+    private final FacilDAO facilDAO
+
+    /*
+    LocationCommand(DiningDAO diningDAO, LocationDAO locationDAO, ExtensionDAO extensionDAO,
+                     ArcGisDAO arcGisDAO, ExtraDataDAO extraDataDAO,
+                     ExtraDataManager extraDataManager, LibraryDAO libraryDAO,
+                     Boolean useHttpCampusMap, CampusMapDAO campusMapDAO, FacilDAO facilDAO) {
+        super("generate", "Generates json files")
+        this.diningDAO = diningDAO
+        this.locationDAO = locationDAO
+        this.extensionDAO = extensionDAO
+        this.arcGisDAO = arcGisDAO
+        this.extraDataDAO = extraDataDAO
+        this.extraDataManager = extraDataManager
+        this.libraryDAO = libraryDAO
+        this.useHttpCampusMap = useHttpCampusMap
+        this.campusMapDAO = campusMapDAO
+        this.facilDAO = facilDAO
+    }
+    */
+
+    LocationCommand() {
+        super("generate", "Generates json files")
+    }
+
+    @Override
+    public void configure(Subparser subparser) {
+        super.configure(subparser)
+        // can add arguments here
+    }
+
+    @Override
+    protected void run(Bootstrap<LocationConfiguration> bootstrap,
+                       Namespace namespace,
+                       LocationConfiguration configuration) throws Exception {
+        System.printf("hi %s\n", configuration.api.endpointUri)
+    }
+
+    /*Response getDining() {
+        final List<ServiceLocation> diningLocations = diningDAO.getDiningLocations()
+
+        if (!diningLocations) {
+            return notFound().build()
+        }
+
+        ResultObject resultObject = writeJsonAPIToFile("locations-dining.json", diningLocations)
+        ok(resultObject).build()
+    }
+
+    Response getFacilities() {
+        ArrayList mergedData = getBuildingData()
+
+        if (!mergedData) {
+            return notFound().build()
+        }
+
+        ResultObject resultObject = writeJsonAPIToFile("locations-arcgis.json", mergedData)
+        ok(resultObject).build()
+    }
+
+    *//**
+     * Returns the combined data from the arcgis and campusmap.
+     *
+     * @return
+     *//*
+    private List getBuildingData() {
+        List<FacilLocation> buildings = facilDAO.getBuildings()
+        // Get acrgis gender inclusive restroom data from http request
+        HashMap<String, GenderInclusiveRRLocation> genderInclusiveRR =
+                arcGisDAO.getGenderInclusiveRR()
+        // Get arcgis coordinate data from json file
+        HashMap<String, ArcGisLocation> arcGisGeometries = locationDAO.getArcGisCoordinates()
+
+        def buildingAndArcGisMerged = locationDAO.mergeFacilAndArcGis(buildings,
+            genderInclusiveRR, arcGisGeometries)
+
+        if (!useHttpCampusMap) {
+            List<CampusMapLocationDeprecated> campusMapLocationList =
+                    locationDAO.getCampusMapFromJson()
+            // Merge the combined arcgis data with campus map data
+            locationDAO.mergeMapAndBuildingsDeprecated(
+                    buildingAndArcGisMerged, campusMapLocationList)
+        } else {
+            new ArrayList<FacilLocation>(buildingAndArcGisMerged.values())
+        }
+    }
+
+    Response getExtension() {
+        final List<ExtensionLocation> extensionLocations = extensionDAO.getExtensionLocations()
+
+        if (!extensionLocations) {
+            return notFound().build()
+        }
+
+        ResultObject resultObject =
+                writeJsonAPIToFile("locations-extension.json", extensionLocations)
+        ok(resultObject).build()
+    }
+
+    Response getLibrary() {
+        def data = libraryDAO.getLibraryHours()
+
+        ok(data).build()
+    }
+
+    Response combineSources() {
+        List<List> locationsList = []
+        locationsList  += getBuildingData()
+        locationsList  += extraDataManager.extraData.locations
+        locationsList  += diningDAO.getDiningLocations()
+        locationsList  += extensionDAO.getExtensionLocations()
+        locationsList  += extraDataDAO.getLocations()
+
+        ResultObject resultObject = writeJsonAPIToFile("locations-combined.json", locationsList)
+        ok(resultObject).build()
+    }
+
+    Response services() {
+        List<List> locationsList = []
+        locationsList  += extraDataDAO.getServices()
+
+        ResultObject resultObject = writeJsonAPIToFile("services.json", locationsList)
+        ok(resultObject).build()
+    }
+
+    *//**
+     * Converts the locations list to result objects that can be returned via the browser. It
+     * also writes the location object list in a json file to be sent to ES.
+     *
+     * @param filename
+     * @param locationsList
+     * @return
+     *//*
+    private ResultObject writeJsonAPIToFile(String filename, List locationsList) {
+        ResultObject resultObject = new ResultObject()
+        def jsonESInput = new File(filename)
+        jsonESInput.write("") // clear out file
+
+        resultObject.data = []
+        locationsList.each {
+            resultObject.data += locationDAO.convert(it)
+        }
+
+        MergeUtil mergeUtil = new MergeUtil(
+                resultObject,
+                libraryDAO,
+                extraDataDAO,
+                campusMapDAO)
+        if (useHttpCampusMap) {
+            mergeUtil.mergeCampusMapData()
+        }
+        if (filename != "services.json") {
+            mergeUtil.merge() // only applies to locations
+            mergeUtil.populate() // only applies to locations for now?
+            mergeUtil.appendRelationshipsToLocations()
+        } else {
+            mergeUtil.appendRelationshipsToServices()
+        }
+
+        // @todo: move this somewhere else
+        ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
+
+        resultObject.data.each {
+            def indexAction = [index: [_id: it.id]]
+            jsonESInput << mapper.writeValueAsString(indexAction) + "\n"
+            jsonESInput << mapper.writeValueAsString(it) + "\n"
+        }
+        resultObject
+    }*/
+}
