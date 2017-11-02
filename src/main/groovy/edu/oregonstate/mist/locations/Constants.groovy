3697151850d27c0bb63a03199a15e4e1e9bce563
Andrew Ekstedt <ekstedta@oregonstate.edu>
2017-11-02 14:24:37 -0700

    add some comments to Constants
    CO-930

--- src/main/groovy/edu/oregonstate/mist/locations/Constants.groovy
+++ src/main/groovy/edu/oregonstate/mist/locations/Constants.groovy
@@ -1,12 +1,18 @@
 package edu.oregonstate.mist.locations
 
 class Constants {
+    // Valid values for Attributes.type
     public static final String TYPE_BUILDING = "building"
     public static final String TYPE_DINING = "dining"
     public static final String TYPE_OTHER = "other"
     public static final String TYPE_SERVICES = "services"
+
+    // Locations frontend endpoint names,
+    // used in constructing URLs
     public static final String SERVICES = "services"
     public static final String LOCATIONS = "locations"
+
+    // Valid values for Attributes.campus
     public static final String CAMPUS_CORVALLIS = "corvallis"
     public static final String CAMPUS_EXTENSION = "extension"
 }
