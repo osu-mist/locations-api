5bb101dc8ae1e83c24eea993fbf814cd11821306
Andrew Ekstedt <ekstedta@oregonstate.edu>
2017-11-02 09:29:37 -0700

    reformat test
    CO-930

--- src/test/groovy/edu/oregonstate/mist/locations/db/ArcGisDAOTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/locations/db/ArcGisDAOTest.groovy
@@ -15,15 +15,40 @@ class ArcGisDAOTest {
 
     @Test
     void testRR() {
-        def testData = '{"features":[{"attributes":{"OBJECTID":80,"BldNam":"Withycombe Hall","BldID":"0075","Notes":" ",' +
-                '"LocaADA":"0071","LocaADA_Not":"0123, 0167A, 0307,169",' +
-                '"LocaAll":"0071, 0123, 0167A, 0307, 0169","BldNamAbr":"With","Limits":" ",' +
-                '"CntADA":1,"CntADA_Not":4,"CntAll":5}}]}'
+        def testData =
+                '{\n' +
+                '   "features":[\n' +
+                '       {\n' +
+                '           "attributes" : {\n' +
+                '               "OBJECTID" : 80, \n' +
+                '               "BldNam" : "Withycombe Hall", \n' +
+                '               "BldID" : "0075", \n' +
+                '               "Notes" : " ", \n' +
+                '               "LocaADA" : "0071", \n' +
+                '               "LocaADA_Not" : "0123, 0167A, 0307,169", \n' +
+                '               "LocaAll" : "0071, 0123, 0167A, 0307, 0169", \n' +
+                '               "BldNamAbr" : "With", \n' +
+                '               "Limits" : " ", \n' +
+                '               "CntADA" : 1, \n' +
+                '               "CntADA_Not" : 4, \n' +
+                '               "CntAll" : 5\n' +
+                '           }, \n' +
+                '           "geometry" : \n' +
+                '           {\n' +
+                '               "x" : 7476856.0816929191, \n' +
+                '               "y" : 340760.45964567363\n' +
+                '           }\n' +
+                '       }\n' +
+                '  ]\n' +
+                '}\n'
         assert dao.mapRR(testData) == ["0075": new GenderInclusiveRRLocation(
-                bldID: "0075", bldNam: "Withycombe Hall", bldNamAbr: "With",
-                giRestroomCount: 5, giRestroomLimit: " ",
-                giRestroomLocations: "0071, 0123, 0167A, 0307, 0169")
-        ]
+                bldID: "0075",
+                bldNam: "Withycombe Hall",
+                bldNamAbr: "With",
+                giRestroomCount: 5,
+                giRestroomLimit: " ",
+                giRestroomLocations: "0071, 0123, 0167A, 0307, 0169"
+        )]
     }
 }
 
