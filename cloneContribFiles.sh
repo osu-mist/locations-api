aef60a6adb339ac2f5b197bede403a65348814c7
Andrew Ekstedt <ekstedta@oregonstate.edu>
2017-12-15 15:50:25 -0800

    don't try to copy campusmap.json from contrib repo
    Was removed in CO-930.

--- cloneContribFiles.sh
+++ cloneContribFiles.sh
@@ -5,7 +5,6 @@ if [ "$2" = "yes" ]
         cd campusmap
         git reset --hard "$3"
         cd "$WORKDIR"
-        mv campusmap/campusmap.json "$WORKDIR"/campusmap.json
         mv campusmap/extra-data.yaml "$WORKDIR"/extra-data.yaml
         rm -rf campusmap/
 fi
