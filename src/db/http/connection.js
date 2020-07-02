45e5664ede1d48fe3d6aabf3e48a1e5e82162da5
David Kalcic <kalcicd@oregonstate.edu>
2020-07-02 10:58:53 -0700

    fix data source validation

--- src/db/http/connection.js
+++ src/db/http/connection.js
@@ -1,14 +1,11 @@
+import awsAuth from 'aws4';
 import config from 'config';
 import rp from 'request-promise-native';
 
 import { logger } from 'utils/logger';
 
-const { baseUri, headers } = config.get('dataSources.http');
-
-const httpOptions = {
-  headers,
-  json: true,
-};
+const { host, accessKeyId, secretAccessKey } = config.get('dataSources.http');
+const httpOptions = { host, path: '/', json: true };
 
 /**
  * Validate http connection and throw an error if invalid
@@ -16,8 +13,10 @@ const httpOptions = {
  * @returns {Promise} resolves if http connection can be established and rejects otherwise
  */
 const validateHttp = async () => {
+  awsAuth.sign(httpOptions, { accessKeyId, secretAccessKey });
   try {
-    await rp.head({ ...{ uri: baseUri }, ...httpOptions });
+    // using GET for authentication temporarily, HEAD returns 403
+    await rp.get({ ...{ uri: `https://${host}` }, ...httpOptions });
   } catch (err) {
     logger.error(err);
     throw new Error('Unable to connect to HTTP data source');
